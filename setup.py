df0546bfca884ef29921a3d245356cbf18dd647d
Lawrence Weikum <lawrencemq@gmail.com>
2023-03-22 09:28:29 -0700

    stubbing alpha release. will document later

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="printipy",
-    version="1.0.0",
+    version="1.0.0-alpha",
     author="Lawrence Weikum",
     description="Printify API for Python",
     url="https://github.com/lawrencemq/printipy",
