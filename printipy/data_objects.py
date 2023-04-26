a7cb901601f99a3ae41606bde3e3a478f4121f07
Lawrence Weikum <lawrencemq@gmail.com>
2023-04-26 14:25:06 -0700

    adding more docs for data objects

--- printipy/data_objects.py
+++ printipy/data_objects.py
@@ -13,6 +13,11 @@ def _exclude_if_none(value):
 class Shop:
     """
     Shop object to store and validate shop data between Python and Printify
+
+    Args:
+        id: Shop ID
+        title: Shop Name
+        sales_channel: Sales Channel, e.g., Etsy, Walmart, etc.
     """
     id: str
     title: str
@@ -22,6 +27,17 @@ class Shop:
 @dataclass_json
 @dataclass
 class Blueprint:
+    """
+    Blueprint object to store and validate shop data between Python and Printify
+
+    Args:
+        id: Blueprint ID
+        title: Name of Blueprint
+        description: Description of the Blueprint given by the provider
+        brand: Brand given by the provider
+        model: Model given by the provider
+        images: List of image URLs given by the provider
+    """
     id: int
     title: str
     description: str
@@ -33,6 +49,17 @@ class Blueprint:
 @dataclass_json
 @dataclass
 class Location:
+    """
+    Location object to store and validate shop data between Python and Printify
+
+    Args:
+        address1: First address line
+        city: City
+        country: Country
+        region: Region
+        zip: Zipcode
+        address2: Second address line. Defaults to None.
+    """
     address1: str
     city: str
     country: str
@@ -44,6 +71,22 @@ class Location:
 @dataclass_json
 @dataclass
 class Address:
+    """
+    Address object to store and validate shop data between Python and Printify. This is similar to `Location` but includes more information and used for specific APIs
+
+    Args:
+        first_name: First name of recipient
+        last_name: Last name of recipient
+        address1: First line of the address
+        city: City
+        country: Country
+        region: Region
+        zip: Zipcode
+        address2: Second line of the address. Defaults to None.
+        email: Email of the recipient. Defaults to None.
+        phone: Phone of the recipient. Defaults to None.
+        company: Company name of the recipient. Defaults to None.
+    """
     first_name: str
     last_name: str
     address1: str
@@ -62,6 +105,14 @@ class Address:
 @dataclass_json
 @dataclass
 class PrintProvider:
+    """
+    Print Provider object to store and validate shop data between Python and Printify.
+
+    Args:
+        id: Provider's ID
+        title: Name of the provider.
+        location: Location of the provider. Defaults to None.
+    """
     id: int
     title: str
     location: Optional[Location] = field(
@@ -72,6 +123,16 @@ class PrintProvider:
 @dataclass_json
 @dataclass
 class VariantOption:
+    """
+    Object representing various options for Variants. Stores and validate data between Python and Printify.
+    Different products and their variants will use different combinations of values.
+
+    Args:
+        color: Color of item. Defaults to None.
+        size: Size of item. Defaults to None.
+        paper: Paper of item. Defaults to None.
+        quantity: Quantity of item. Defaults to None.
+    """
     color: Optional[str] = field(
         default=None, metadata=config(exclude=_exclude_if_none)
     )
@@ -89,6 +150,14 @@ class VariantOption:
 @dataclass_json
 @dataclass
 class VariantPlaceholder:
+    """
+    Object representing the Placeholder for a product variant. Stores and validate data between Python and Printify.
+
+    Args:
+        position: Position of artwork
+        height: Height of artwork
+        width: Width of artwork
+    """
     position: str
     height: int
     width: int
@@ -97,6 +166,15 @@ class VariantPlaceholder:
 @dataclass_json
 @dataclass
 class Variant:
+    """
+    Object representing a Variant for a product. Stores and validate data between Python and Printify.
+
+    Args:
+        id: Variant ID
+        title: Name of the variant
+        options: Options given to the variant
+        placeholders: List of placeholders for the artwork
+    """
     id: int
     title: str
     options: VariantOption
@@ -106,17 +184,36 @@ class Variant:
 @dataclass_json
 @dataclass
 class PrintProviderVariants:
+    """
+    Object representing a Variant from a print provider. Stores and validate data between Python and Printify.
+
+    Args:
+        id:  Provider ID
+        title: Name of provider
+        variants: List of variants provider offers
+    """
     id: int
     title: str
     variants: List[Variant]
 
-    def get_variant_ids(self):
+    def get_variant_ids(self) -> List[int]:
+        """
+        Returns a list of all IDs from the associated variants
+        """
         return [x.id for x in self.variants]
 
 
 @dataclass_json
 @dataclass
 class ShippingInfoHandlingTime:
+    """
+    Object representing the handling time for a given shipping option from a print provider.
+    Stores and validate data between Python and Printify.
+
+    Args:
+        value: The amount of time
+        unit: The unit of time
+    """
     value: int
     unit: str
 
@@ -124,6 +221,14 @@ class ShippingInfoHandlingTime:
 @dataclass_json
 @dataclass
 class ShippingInfoProfileCost:
+    """
+    Object representing the shipping cost for an item from a print provider.
+    Stores and validate data between Python and Printify.
+
+    Args:
+        cost: shipping cost in whole values. E.g., $12.98 would be 1298
+        currency: currency of the shipping cost, e.g., USD
+    """
     cost: int
     currency: str
 
@@ -131,6 +236,16 @@ class ShippingInfoProfileCost:
 @dataclass_json
 @dataclass
 class ShippingInfoProfile:
+    """
+    Object representing the shipping profile a group of items to a given set of countries from a print provider.
+    Stores and validate data between Python and Printify.
+
+    Args:
+        variant_ids: A list of variants of a given product
+        first_item: cost to ship the first item
+        additional_items: cost to ship any additional items to the first item
+        countries: a list of country codes this shipping profile applies to
+    """
     variant_ids: List[int]
     first_item: ShippingInfoProfileCost
     additional_items: ShippingInfoProfileCost
@@ -140,6 +255,14 @@ class ShippingInfoProfile:
 @dataclass_json
 @dataclass
 class ShippingInfo:
+    """
+    Object representing all shipping information for a group of items from a print provider.
+    Stores and validate data between Python and Printify.
+
+    Args:
+        handling_time: Handling time information before an item is shipped
+        profiles: List of shipping information. Includes various prices to different countries.
+    """
     handling_time: ShippingInfoHandlingTime
     profiles: List[ShippingInfoProfile]
 
@@ -147,6 +270,14 @@ class ShippingInfo:
 @dataclass_json
 @dataclass
 class ShippingCost:
+    """
+    Object representing all shipping costs from a print provider.
+    Stores and validate data between Python and Printify.
+
+    Args:
+        standard: cost of shipping given standard shipping
+        express: cost of shipping given express shipping
+    """
     standard: int
     express: int
 
@@ -154,6 +285,17 @@ class ShippingCost:
 @dataclass_json
 @dataclass
 class ShippingEstimateLineItemByProduct:
+    """
+    Object representing a shipping estimate for an item based on its product and variant information.
+    Stores and validate data between Python and Printify.
+
+    Used for `CreateShippingEstimate`
+
+    Args:
+        product_id: ID of a product already created in a shop
+        variant_id: Variant ID of that product
+        quantity: Number of items to send
+    """
     product_id: str
     variant_id: int
     quantity: int
@@ -162,6 +304,18 @@ class ShippingEstimateLineItemByProduct:
 @dataclass_json
 @dataclass
 class ShippingEstimateLineItemByVariant:
+    """
+    Object representing a shipping estimate for a new item based on its variant information.
+    Stores and validate data between Python and Printify.
+
+    Used for `CreateShippingEstimate`
+
+    Args:
+        print_provider_id: ID of the Print Provider
+        blueprint_id: ID of the Blueprint of the product from the provider
+        variant_id: ID of the variant from the blueprint given from the producer
+        quantity: Positive integer of items
+    """
     print_provider_id: int
     blueprint_id: int
     variant_id: int
@@ -171,6 +325,16 @@ class ShippingEstimateLineItemByVariant:
 @dataclass_json
 @dataclass
 class ShippingEstimateLineItemBySku:
+    """
+    Object representing a shipping estimate for an item based on SKU number and quantity.
+    Stores and validate data between Python and Printify.
+
+    Used for `CreateShippingEstimate`
+
+    Args:
+        sku: SKU number of item
+        quantity: Number of items to send
+    """
     sku: str
     quantity: int
 
@@ -178,6 +342,14 @@ class ShippingEstimateLineItemBySku:
 @dataclass_json
 @dataclass
 class CreateShippingEstimate:
+    """
+    Object representing a shipping estimate for a list of items to a given address from a print provider.
+    Stores and validate data between Python and Printify.
+
+    Args:
+        line_items: List of items. This can be a list of products, a list of items by their SKU, or a list of new products given their variant details.
+        address_to: Address of the recipient
+    """
     line_items: List[
         Union[ShippingEstimateLineItemByProduct, ShippingEstimateLineItemByVariant, ShippingEstimateLineItemBySku]]
     address_to: Address
@@ -295,6 +467,19 @@ class Product:
 @dataclass_json
 @dataclass
 class Publish:
+    """
+    Object that tells Printify what to publish to a shop for a given product.
+    Stores and validate data between Python and Printify.
+
+    Args:
+        title: True if the title in the storefront should be updated to that in Printify
+        description: True if the description in the storefront should be updated to that in Printify
+        images: True if the images in the storefront should be updated to that in Printify
+        variants: True if the variants in the storefront should be updated to that in Printify
+        tags: True if the tags in the storefront should be updated to that in Printify
+        keyFeatures: True if the keyFeatures in the storefront should be updated to that in Printify
+        shipping_template: True if the shipping_template in the storefront should be updated to that in Printify
+    """
     title: bool = True
     description: bool = True
     images: bool = True
