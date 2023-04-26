a0d2c47cdda8d2c5651ec948947e307500d8f3c1
Lawrence Weikum <lawrencemq@gmail.com>
2023-04-26 14:39:10 -0700

    stubbing remaining work in exceptions

--- printipy/exceptions.py
+++ printipy/exceptions.py
@@ -1,18 +1,33 @@
 class PrintiPyException(Exception):
+    """
+    TODO
+    """
     pass
 
 
 class ParseException(PrintiPyException):
+    """
+    TODO
+    """
     pass
 
 
 class InvalidScopeException(Exception):
+    """
+    TODO
+    """
     pass
 
 
 class InvalidRequestException(Exception):
+    """
+    TODO
+    """
     pass
 
 
 class PrintifyException(Exception):
+    """
+    TODO
+    """
     pass
